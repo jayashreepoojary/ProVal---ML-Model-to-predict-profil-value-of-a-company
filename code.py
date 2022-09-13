df567b2590ce463f442bffdd4b9ef92a779a6dd5
Jayashree <jayashreepoojary1506@gmail.com>
2022-09-13 20:22:47 +0530

    first commit

---
+++ code.py
@@ -0,0 +1,29 @@
+#importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#reading data from csv file
+data = pd.read_csv(r"C:\Users\Admin\Desktop\Exposys datalabs\50_Startups.csv")
+print(data.head())
+print(data.describe())
+#using seaborn library for graphics
+sns.heatmap(data.corr(), annot=True)
+plt.show()
+#plotting
+#using linear regression model algorithm to train
+x = data[["R&D Spend", "Administration", "Marketing Spend"]]
+y = data["Profit"]
+x = x.to_numpy()
+y = y.to_numpy()
+y = y.reshape(-1, 1)
+#using sklearn library
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+#training the linear regression model
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(xtrain, ytrain)
+ypred = model.predict(xtest)
+data = pd.DataFrame(data={"Predicted Profit": ypred.flatten()})
+print(data.head())
